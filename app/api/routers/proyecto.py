0de5c024df8ba7d1ce3e60fb867ec7e5daf7078c
JULEN ALVARO ESPINO <jalvaro@caf.net>
2023-10-31 12:52:20 +0100

    router de proyectos OK

--- app/api/routers/proyecto.py
+++ app/api/routers/proyecto.py
@@ -8,13 +8,6 @@ from app.db.session import get_db
 
 router_proyecto = APIRouter()
 
-# @router_proyecto.post("/proyectos/", response_model=schemas.Proyecto)
-# def create_proyecto(proyecto: schemas.ProyectoCreate, db: Session = Depends(get_db)):
-#     db_proyecto = crud.get_proyecto(db, proyecto_id=proyecto.id_proyecto)
-#     if db_proyecto:
-#         raise HTTPException(status_code=400, detail="Proyecto ya registrado")
-#     return crud.create_proyecto(db=db, proyecto=proyecto)
-
 @router_proyecto.get("/proyectos/", response_model=List[schemas.Proyecto])
 def read_proyectos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     proyectos = crud.get_proyectos(db, skip=skip, limit=limit)
