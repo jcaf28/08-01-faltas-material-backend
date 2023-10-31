88ab33081b37d07def0e759eb3fd1e3d7587527a
JULEN ALVARO ESPINO <jalvaro@caf.net>
2023-10-31 09:52:08 +0100

    creando primeros endpoints, antes de intentar convertir importar_excel_kits en una función asíncrona

--- app/services/db/clean_db.py
+++ app/services/db/clean_db.py
@@ -1,21 +1,2 @@
 # app/services/db/clean_db.py
 
-# app/db/clean_db.py
-
-from sqlalchemy.orm import Session
-from app.db.session import engine
-from app.db.base import Base
-import click
-
-@click.command()
-def clean_db():
-    """
-    Borrar todos los datos de la base de datos.
-    """
-    with Session(engine) as session:
-        for table in reversed(Base.metadata.sorted_tables):
-            session.execute(table.delete())
-        session.commit()
-
-if __name__ == "__main__":
-    clean_db()
